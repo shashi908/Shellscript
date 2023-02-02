b1b5ea0b35fef10f4d59f337e29b4171c31ef2d8
shashi908 <shashi.chanda123@gmail.com>
2023-02-02 21:26:39 +0530

    Shell script to find first 10 biggest files

---
+++ shell1.sh
@@ -0,0 +1,11 @@
+************************************************************************************
+Script Description : Shell script to find first 10 biggest file in filesystem and redirect them to a file.
+Written by : Shashi Kiran
+Contact : 
+************************************************************************************
+# Identify the first 10 biggest files using du -a in /
+# Using sort -n -r to sort out the output
+# using the head to list top 10 files
+# using >> to send the output to a file.
+sudo du -a / | sort -n -r | head -n 10 >> /home/biggestfilesize.txt
+
